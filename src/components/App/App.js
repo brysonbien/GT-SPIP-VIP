9a88e1d8f86d685c25fe5730f9ea21083ba3a7b3
Kim, Bryan Kyunghun <bkim441@gatech.edu>
2022-03-12 13:39:22 -0500

    Login commit

---
+++ src/components/App/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import './App.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Dashboard from '../Dashboard/Dashboard';
+import Login from '../Login/Login';
+import Preferences from '../Preferences/Preferences';
+import useToken from './useToken';
+
+
+function App() {
+  const { token, setToken } = useToken();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+  return (
+    <div className="wrapper">
+      <h1>Georgia Tech MBB VIP</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/preferences">
+            <Preferences />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
