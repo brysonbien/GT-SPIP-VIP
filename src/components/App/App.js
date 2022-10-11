f85c3185c1708957bd33a96a25b3746dfa776714
Adityaa Magesh Kumar <adityaa.magesh@gmail.com>
2022-10-11 10:05:03 -0400

    fixed login

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,37 +1,36 @@
-import React, { useState } from 'react';
-import './App.css';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import Dashboard from '../Dashboard/Dashboard';
-import Login from '../Login/Login';
-import Preferences from '../Preferences/Preferences';
-import useToken from './useToken';
-import Homepage from '../Homepage/Homepage';
+import React, { useState } from "react";
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Dashboard from "../Dashboard/Dashboard";
+import Login from "../Login/Login";
+import Preferences from "../Preferences/Preferences";
+import useToken from "./useToken";
+import Homepage from "../Homepage/Homepage";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
-import gtmensbasketballbanner from "../Homepage/gtmensbasketballbanner.png"
-
-
+import gtmensbasketballbanner from "../Homepage/gtmensbasketballbanner.png";
 
 function App() {
-  const { token, setToken } = useToken();
+	const { token, setToken } = useToken();
 
-  // if(!token) {
-  //   return <Login setToken={setToken} />
-  // }
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route path="/" exact component={Homepage} />
-          <Route path="/dashboard" exact component={Dashboard} />
-          {/* <Route path="/about" exact component={About} />
+	// if(!token) {
+	//   return <Login setToken={setToken} />
+	// }
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Navbar />
+				<Switch>
+					<Route path="/" exact component={Homepage} />
+					<Route path="/login" exact component={Login} />
+					<Route path="/dashboard" exact component={Dashboard} />
+					{/* <Route path="/about" exact component={About} />
           <Route path="/contact" exact component={Contact} /> */}
-        </Switch>
-        <Footer />
-      </BrowserRouter>
-    </div>
-  );
+				</Switch>
+				<Footer />
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import Login from '../Login/Login';
-import gtmensbasketballbanner from "../Homepage/gtmensbasketballbanner.png"
+import React from "react";
+import Login from "../Login/Login";
+import gtmensbasketballbanner from "../Homepage/gtmensbasketballbanner.png";
 import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 import "../Homepage/Homepage.css";
 
 export default function Homepage() {
-    return ( 
-        //make the welcome text not right ce
-        <div className="home" style={{ backgroundImage: `url(${gtmensbasketballbanner})` }}>
-            <div className="headerContainer">
-                <h1>Sports Intelligence Platform</h1>
-        
-                <p>Welcome!</p>
-            <Link to="/Dashboard">
-                <button> Continue </button>
-            </Link>
-            </div>
-        </div>
-        
-        
-    );
+	return (
+		//make the welcome text not right ce
+		<div
+			className="home"
+			style={{ backgroundImage: `url(${gtmensbasketballbanner})` }}
+		>
+			<div className="headerContainer">
+				<h1>Sports Intelligence Platform</h1>
+
+				<p>Welcome!</p>
+				<Link to="/login">
+					<button> Continue </button>
+				</Link>
+			</div>
+		</div>
+	);
 }
